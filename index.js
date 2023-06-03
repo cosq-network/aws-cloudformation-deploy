7044694ca62147c37b086cafb192bc83b858be88
Benoy Bose <benoybose@gmail.com>
2023-06-03 16:43:21 +0530

    Trying to read Github env to pass parameters

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const { execFile } = require('node:child_process');
 const core = require('@actions/core');
 const { stdout, stderr } = require('node:process');
+const YAML = require('yaml');
+const fs = require('fs');
+const process = require('node:process');
+
 const templateFile = core.getInput('template-file', { required: true });
 const stackName = core.getInput('stack-name', { required: true });
 const parameterOverrides = core.getInput('parameters-json', { required: false });
 const capabilityIAM = core.getInput('capability-iam', { required: false });
 const capabilityNamedIAM = core.getInput('capability-named-iam', { required: false });
+
+const templateFileContent = fs.readFileSync(templateFile);
+core.info(templateFileContent);
+const templateObject = YAML.parse(templateFileContent);
+core.info(templateObject);
+if (!!templateObject['Parameters']) {
+    const githubEnv = process.env['GITHUB_ENV'];
+    if (!!githubEnv) {
+        core.info(githubEnv);
+        const githubEnvObject = YAML.parse(githubEnv);
+        core.info(githubEnvObject);
+    }
+    for (const parameterName in templateObject['Parameters']) {
+        core.info(`parameter found: $(parameterName)`);
+    }
+}
 
 const arguments = ['cloudformation', 'deploy'];
 arguments.push('--template-file');
