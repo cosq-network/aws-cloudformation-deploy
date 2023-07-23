7eb06a57211a3d2092980a837c70609ae1733a89
Benoy Bose <benoybose@gmail.com>
2023-07-23 13:52:54 +0530

    Updating actions

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# aws-cloudformation-deploy
-Github action to deploy a Stack to the AWS CloudFormation
+# CloudFormation Deploy Action
+
+This GitHub Action creates or updates an AWS CloudFormation stack using the AWS CLI. It accepts the following inputs:
+
+- `stackName`: Name of the stack to be passed to the `--stack-name` argument of the AWS CLI.
+- `templateFile`: Path to the template file to be passed to the `--template-file` argument of the AWS CLI.
+- `parameterFile`: Path to a JSON parameter file to be pre-processed and passed to the `--parameter-overrides` argument of the AWS CLI.
+- `hasIAMCapability`: Determines whether to pass `CAPABILITY_IAM` as a capability. (Optional, default: `false`)
+- `hasIAMNamedCapability`: Determines whether to pass `CAPABILITY_NAMED_IAM` as a capability. (Optional, default: `false`)
+
+The `parameterFile` is pre-processed so that all patterns like `$(VAR)` inside the file are replaced by respective environment variables with the same name (`VAR`) in the GitHub Action context.
+
+Once the stack is successfully created or updated, the action retrieves the outputs of the stack using the `aws cloudformation describe-stacks` command. Each output is then set as an environment variable in GitHub with the same name as the output name and value.
+
+## Example usage
+
+```yaml
+on: [push]
+
+jobs:
+  deploy_stack:
+    runs-on: ubuntu-latest
+    steps:
+    - name: Checkout repository
+      uses: actions/checkout@v2
+
+    - name: Deploy CloudFormation Stack
+      uses: your-username/cloudformation-deploy-action@v1
+      with:
+        stackName: 'MyCloudFormationStack'
+        templateFile: 'path/to/cloudformation-template.yml'
+        parameterFile: 'path/to/parameter-file.json'
+        hasIAMCapability: true
+        hasIAMNamedCapability: false
--- dist/action.js
+++ dist/action.js
@@ -1,7 +1,6 @@
 const core = require('@actions/core');
 const child_process = require('child_process');
 const fs = require('fs');
-
 async function run() {
   try {
     const stackName = core.getInput('stackName');
@@ -14,7 +13,6 @@ async function run() {
     let parameterOverrides = fs.readFileSync(parameterFile, 'utf8');
     const parameterRegex = /\$\((.*?)\)/g;
     const matches = parameterOverrides.match(parameterRegex);
-
     if (matches) {
       for (const match of matches) {
         const envVariable = match.slice(2, -1);
@@ -26,21 +24,21 @@ async function run() {
 
     // Run AWS CLI command to create or update the stack
     const deployArgs = ['cloudformation', 'deploy', '--stack-name', stackName, '--template-file', templateFile, '--parameter-overrides', parameterOverrides];
-
     if (hasIAMCapability) {
       deployArgs.push('--capabilities', 'CAPABILITY_IAM');
     }
-
     if (hasIAMNamedCapability) {
       deployArgs.push('--capabilities', 'CAPABILITY_NAMED_IAM');
     }
-
-    child_process.execFileSync('aws', deployArgs, { stdio: 'inherit' });
+    child_process.execFileSync('aws', deployArgs, {
+      stdio: 'inherit'
+    });
 
     // Get the stack outputs using AWS CLI and set them as environment variables
     const describeStacksArgs = ['cloudformation', 'describe-stacks', '--stack-name', stackName];
-    const describeStacksOutput = child_process.execFileSync('aws', describeStacksArgs, { encoding: 'utf8' });
-
+    const describeStacksOutput = child_process.execFileSync('aws', describeStacksArgs, {
+      encoding: 'utf8'
+    });
     const stacks = JSON.parse(describeStacksOutput).Stacks;
     if (stacks.length > 0 && stacks[0].Outputs) {
       for (const output of stacks[0].Outputs) {
@@ -50,11 +48,9 @@ async function run() {
         console.log(`set output ${outputKey} and ${outputValue}`);
       }
     }
-
     core.setOutput('stackOutputs', describeStacksOutput);
   } catch (error) {
     core.setFailed(error.message);
   }
 }
-
 run();
